c35d01f164f7a40e3935e7615ebf69d30e0bf425
Kris <heijnenkris@gmail.com>
2022-01-20 17:54:21 +0100

    create theme provider in _app.js

---
+++ themes/main.js
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#6253cb",
+    },
+    secondary: {
+      main: "#9ad88e",
+    },
+  },
+});
+
+export default theme;

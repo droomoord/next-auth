33116820f2276213d302a75ab8cf1dec17c42901
Kris <heijnenkris@gmail.com>
2022-01-22 16:03:55 +0100

    make withoutauth check if user exists

--- HOC/withoutauth.js
+++ HOC/withoutauth.js
@@ -1,18 +1,26 @@
 import jwt from "jsonwebtoken";
+import User from "../models/user";
+import dbConnect from "../lib/dbConnect";
 
 const WithoutAuth = (gssp) => {
   return async (context) => {
+    const redirect = (destination) => {
+      return {
+        redirect: {
+          destination,
+          statusCode: 302,
+        },
+      };
+    };
     try {
       const { req } = context;
       if (req.cookies.jwt) {
         const decode = jwt.verify(req.cookies.jwt, process.env.JWT_SECRET);
         if (decode) {
-          return {
-            redirect: {
-              destination: "/dashboard",
-              statusCode: 302,
-            },
-          };
+          await dbConnect();
+          const id = decode.user;
+          const user = await User.findById(id);
+          if (user) return redirect("/dashboard");
         }
       }
       return await gssp(context);
